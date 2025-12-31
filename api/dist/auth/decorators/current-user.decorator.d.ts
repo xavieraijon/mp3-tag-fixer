e5eec302bc5a4adb510a7dba10dfde059de9b94a
Xavi Aijon <xavier.aijon@col.vueling.com>
2025-12-31 13:05:34 +0100

    Limpiar archivos compilados y actualizar estilos del editor de tags

--- api/dist/auth/decorators/current-user.decorator.d.ts
+++
@@ -1,2 +0,0 @@
-import { ClerkUser } from '../clerk.service';
-export declare const CurrentUser: (...dataOrPipes: (keyof ClerkUser | import("@nestjs/common").PipeTransform<any, any> | import("@nestjs/common").Type<import("@nestjs/common").PipeTransform<any, any>> | undefined)[]) => ParameterDecorator;
--- api/dist/auth/guards/clerk-auth.guard.d.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-import { ClerkService } from '../clerk.service';
-export declare class ClerkAuthGuard implements CanActivate {
-    private clerkService;
-    private reflector;
-    constructor(clerkService: ClerkService, reflector: Reflector);
-    canActivate(context: ExecutionContext): Promise<boolean>;
-}
